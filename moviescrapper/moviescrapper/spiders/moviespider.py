75206573e70d0140f6790e8f008e53d3e3fcc0f7
alsongard <alsongard@gmail.com>
2025-01-25 23:18:11 +0200

    getting data from next page : 3

--- moviescrapper/moviescrapper/spiders/moviespider.py
+++ moviescrapper/moviescrapper/spiders/moviespider.py
@@ -15,5 +15,11 @@ class MoviespiderSpider(scrapy.Spider):
                 'movie_name': movie.css("div.browse-movie-bottom  a::text").get(),
                 'movie_rating': movie.css("h4.rating::text").get(),
                 'movie_url' : movie.css("div.browse-movie-bottom a::attr(href)").get(),
-                'movie_release_year' : movie.css("div.browse-movie-bottom div::text").get()
+                'movie_release_year' : movie.css("div.browse-movie-bottom div::text").get(),
+                'movie_category' : movie.xpath("//figcaption/h4[not(@class='rating')]/text()").get() #or you could use following sibliing
             }
+
+        next_page = response.xpath("//section/following-sibling::*[1]/ul/li[11]/a/@href").get()
+        if next_page is not None:
+            next_page_url = "https://yts.mx" + next_page
+            yield response.follow(next_page_url, callback=self.parse) 
