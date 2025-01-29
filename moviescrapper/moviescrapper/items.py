12ea673c93ecc55f5c074fa34195d424898510d4
alsongard <alsongard@gmail.com>
2025-01-30 01:07:15 +0200

    items class configured and working

--- moviescrapper/moviescrapper/items.py
+++ moviescrapper/moviescrapper/items.py
@@ -4,9 +4,18 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+import scrapy.item
 
 
 class MoviescrapperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
+
+
+class MovieItem(scrapy.Item):
+    movie_name = scrapy.Field()
+    movie_rating = scrapy.Field()
+    movie_url = scrapy.Field()
+    movie_release_year = scrapy.Field()
+    movie_category = scrapy.Field()
