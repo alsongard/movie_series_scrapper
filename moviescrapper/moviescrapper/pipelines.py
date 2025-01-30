3fbdf1822a3327a919050be531cea463fcf9e410
alsongard <alsongard@gmail.com>
2025-01-30 09:25:34 +0200

    performed data postprocessing and complete

--- moviescrapper/moviescrapper/pipelines.py
+++ moviescrapper/moviescrapper/pipelines.py
@@ -10,4 +10,22 @@ from itemadapter import ItemAdapter
 
 class MoviescrapperPipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name == "movie_rating":
+                value = adapter.get(field_name)
+                if value is not None:
+                    new_value= value.replace("/ 10", "").strip()
+                    movie_rate = float(new_value)
+                    adapter[field_name] = movie_rate
+
+        for field_name in field_names:
+            if field_name == "movie_release_year":
+                value = adapter.get(field_name)
+                new_value = int(value)
+                adapter[field_name] = new_value
+
         return item
