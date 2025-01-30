a63210cc64d66ca69e353ab37188f5619bd1823a
alsongard <alsongard@gmail.com>
2025-01-30 10:37:19 +0200

    analysing using streamlit

--- movie_streamlit.py
+++ movie_streamlit.py
@@ -2,23 +2,27 @@ import streamlit as st
 import pandas as pd
 st.write("# YTS.MX MOVIE RATINGS")
 
-st.write("best movies")
 
-data_df = pd.read_csv("./moviescrapper/moviescrapper/movieData.csv")
-
-data_df
+data_df = pd.read_csv("./moviescrapper/new_movies.csv")
 
 # st.write(data_df["movie_rating"].dtype)
 nan_rows = data_df[data_df.isna().any(axis=1)]
-st.write(nan_rows)
+# st.write(f"rows with nan valuesn : {len(nan_rows)} ") # 21
+# st.write(nan_rows)
 
 clean_data_df = data_df.dropna()
-clean_data_df
+# clean_data_df
+
+## get maximum values
+st.write(f" Highest Rating : {clean_data_df["movie_rating"].max()} ")
+st.write(f" Lowest Rating : {clean_data_df["movie_rating"].min()}")
+
+# get top movies
+st.write("TOP BEST MOVIES")
+best_movies_df = clean_data_df[clean_data_df["movie_rating"] > 8]
+best_movies_df
 
 
-clean_data_df["movie_rating"] = clean_data_df["movie_rating"].astype(str)
+# check the number of  ratings for each rate using value_counts
+st.write(clean_data_df["movie_rating"].value_counts())
 
-clean_data_df["movie_rating"] = clean_data_df["movie_rating"].apply(lambda rating : rating.replace("/10", ""))
-
-st.write("movie_rating new datatype")
-clean_data_df
