8fb87b98ebe1a7b54806ff007af67f29e9c03921
alsongard <alsongard@gmail.com>
2025-02-13 01:41:21 +0200

    done with analysing waiting for hosting

--- movie_streamlit.py
+++ movie_streamlit.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import altair as alt
 st.write("# YTS.MX MOVIE RATINGS")
 
 
@@ -15,18 +18,27 @@ clean_data_df = data_df.dropna()
 # clean_data_df
 
 ## get maximum values
-st.write(f" Highest Rating : {clean_data_df["movie_rating"].max()} ")
-st.write(f" Lowest Rating : {clean_data_df["movie_rating"].min()}")
+highest_rating = clean_data_df["movie_rating"].max()
+lowest_rating = clean_data_df["movie_rating"].min()
+
+st.text(f"""Movie Ratings \n \t Highest Rating: {highest_rating} \n \t Lowest Rating: {lowest_rating}""")
+
+# check the number of  ratings for each rate using value_counts
+# st.write(clean_data_df["movie_rating"].value_counts())
+
 
 # get top movies
 st.write("TOP BEST MOVIES")
-best_movies_df = clean_data_df[clean_data_df["movie_rating"] > 8]
+best_movies_df = clean_data_df[clean_data_df["movie_rating"] >= 8].reset_index(drop=True)
+# best_movies_df
+best_movies_df = best_movies_df.sort_values(by=["movie_rating"], ascending=False).reset_index(drop=True)
 best_movies_df
+# print(type(best_movies_df))
+# st.write("Top Movie Category based on best movies with ratings higher than 8")
+st.write("The chart belows shows the which movie category has the highest number of movies with high ratings.")
+# st.write(best_movies_df["movie_category"].value_counts())
 
-
-# check the number of  ratings for each rate using value_counts
-st.write(clean_data_df["movie_rating"].value_counts())
-
-
-
-movie_category = input("Enter category of movie you would want: ")
+best_movies_category_ratings = best_movies_df["movie_category"].value_counts().rename_axis('movie_category').reset_index(name="count")
+st.bar_chart(best_movies_category_ratings, y="count", x="movie_category", y_label="Highest Number")
+best_movies_category_ratings
+st.write("You have 8 movies in the category ACTION that have a rating of above 8")
