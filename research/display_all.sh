9d98cbf4c7962fd092c82378d8fd846ec30ad769
alsongard <alsongard@gmail.com>
2025-02-20 02:32:30 +0300

    scripting files added

---
+++ research/display_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /home/alson-kali/PROGRAMMING/movie_series_scrapper
+source bin/activate
+cd moviescrapper
+scrapy crawl moviespider
+
+
+:'
+# bash scripting
+bash scripting is a file that contain multiple commands that are executed line by line by the bash program.
+
+shell: a command that provides a command line interface for interacting with the operating system.
+Bash: One of the commonly used Unix/Linux shell is bash ?? questionable
+'
