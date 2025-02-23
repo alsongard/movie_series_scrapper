d86894360a586be49431aeb7c9ce1d9a81a95d11
alsongard <alsongard@gmail.com>
2025-02-23 12:17:15 +0300

    updated web

--- user_streamlit_page.py
+++ user_streamlit_page.py
@@ -67,9 +67,33 @@ st.text(f"""Movie Ratings \n \t Highest Rating: {highest_rating} \n \t Lowest Ra
 
 movie_categories = ["Action", "Drama", "Comedy", "Horror", "Adventure", "Crime", "Sci-Fi", "Documentary"]
 if entered_option == "year":
-    movie_data_year_based_df = clean_data_df.sort_values(by=["movie_release_year"], ascending=False).reset_index(drop=True)
-    movie_data_year_based_df
+    movie_data_year_sorted_df = clean_data_df.sort_values(by=["movie_release_year"], ascending=False).reset_index(drop=True)
+    # displaying movies after being sorted
+    # movie_data_year_sorted_df
 
+    movie_years = ["2024", "2023", "2025", "2022", "2017", "2015", "2016", "2018", "2019", "2014", "2021", "2010", "2020", "2012", "2004", "2009", "2013", "2006", "2007", "1999", "2011", "1986", "2001", "1985", "2008", "1995", "1989", "1990", "1998", "1991", "1987", "2000", "1966", "1988", "1956", "1997", "1973", "1982", "1984", "1992", "2002", "1968", "1970", "2005", "1972", "1934", "1964", "1976", "1967", "1930", "1977", "1958", "1978", "1959", "1962", "1971", "1965", "1979", "1950", "1969", "1983", "1996", "1926", "1975", "1961", "1931", "1954", "1994", "1980", "1938", "1923", "1948", "1928", "1949", "2003", "1947", "1939", "1946", "1920", "1974", "1929", "1993", "1951", "1981", "1933", "1937",
+"1936"]
+    movie_years.sort(reverse=True)
+    default_value = movie_years.index("2025")
+    yes_no_options = ["yes", "no"]
+    yes_no_options_default = yes_no_options.index("yes")
+    year_based_answer = st.sidebar.selectbox(f"View movies based on the year", movie_years, index=default_value)
+    rating_option_answer = st.sidebar.selectbox("Rate movies in descending order", yes_no_options, index=yes_no_options_default)
+    # st.sidebar.text(f"year_based_answer : {year_based_answer} and type : {type(year_based_answer)}")
+    year = int(year_based_answer)
+    # st.sidebar.text(f"Year value: {year} : type {type(year)}")
+    # st.sidebar.text(f"rating_option_answer : {rating_option_answer}")
+
+
+    for value in movie_years:
+        year_value = int(value)
+        if year_value == year:
+            year_selected_movies_df = movie_data_year_sorted_df[movie_data_year_sorted_df["movie_release_year"] == year_value].reset_index(drop=True)
+            if rating_option_answer == "yes":
+                year_selected_rating_sorted_df = year_selected_movies_df.sort_values(by=["movie_rating"], ascending=False).reset_index(drop=True)
+                year_selected_rating_sorted_df
+            else:
+                year_selected_movies_df
 else:
     category_entered = st.sidebar.selectbox("Select the category of the movie you wish to view:", movie_categories)
     movie_data_category_based_df = clean_data_df[clean_data_df["movie_category"] == category_entered].reset_index(drop=True)
